db0f70243e122d9ef490fbbbb3196bfe8b0464ce
MillerRen <ren12345@126.com>
2022-03-03 13:54:57 +0800

    revert

--- js/modules/Workspace.js
+++ js/modules/Workspace.js
@@ -5,8 +5,6 @@ import Tool from './Tool'
 export default class Workspace {
   menubar = null
   mainArea = null
-  toolArea  = null
-  canvasArea = null
   canvas = null
 
   constructor (data) {
@@ -18,8 +16,8 @@ export default class Workspace {
     this.createMenubar()
     this.createMainArea()
     this.createSidePanel()
-    this.createTool()
     this.createEditor()
+    this.createTool()
     this.createInspector()
     this.render()
   }
@@ -31,16 +29,12 @@ export default class Workspace {
   createMainArea () {
     this.mainArea = new LiteGUI.Area({
       id: 'main_area',
-      content_id: 'editor_area',
+      content_id: 'canvas_area',
       main: true,
       height: 'calc( 100% - 32px )',
       inmediateResize: true
     })
     this.mainArea.split('horizontal', [null, 300], true)
-    this.editorArea = this.mainArea.getSection(0)
-    this.editorArea.split('horizontal', [40, null])
-    this.toolArea = this.editorArea.getSection(0)
-    this.canvasArea = this.editorArea.getSection(1)
     LiteGUI.add(this.mainArea)
   }
 
@@ -50,14 +44,14 @@ export default class Workspace {
     this.canvas.style.width = '100%'
     this.canvas.style.height = '100%'
     this.canvas.style.background = '#ffffff'
-    this.canvasArea.content.appendChild(this.canvas)
+    this.mainArea.content.appendChild(this.canvas)
     this.mainArea.onresize = () => this.resize()
     paper.setup(this.canvas)
   }
 
   createTool () {
     this.tool = new Tool()
-    this.toolArea.content.appendChild(this.tool.root)
+    this.mainArea.content.appendChild(this.tool.root)
   }
 
   createSidePanel () {
